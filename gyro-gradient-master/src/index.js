62cbf52382fa05e2376dae20dd10bb7016997683
dan <dbpedersen@gmail.com>
2022-07-04 13:59:49 -0700

    d
    d

--- gyro-gradient-master/src/index.js
+++ gyro-gradient-master/src/index.js
@@ -320,4 +320,14 @@ if (isSupported()) {
   init();
 }
 
+$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
+  
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+  
+  $('.card').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+});
+
 export default createGyroGradient;
